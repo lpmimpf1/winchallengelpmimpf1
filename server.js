a46300cfdfc5a00ed96269ae38426c0abdf41eac
lpmimpf1 <lpmimpf1@gmail.com>
2024-08-06 15:27:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,51 +1,59 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIO(server);
 
 let games = [];
 let timers = { red: 0, blue: 0 };
-let timerIntervals = { red: null, blue: null };
 
-app.use(express.static('public'));
+app.use(express.static('public')); // Statische Dateien aus dem Ordner 'public' bereitstellen
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
+    // Send initial state to the connected client
     socket.emit('update-games', games);
     socket.emit('update-timer', { team: 'red', time: timers.red });
     socket.emit('update-timer', { team: 'blue', time: timers.blue });
 
-    socket.on('start-timer', (team) => {
-        if (timerIntervals[team] === null) {
-            const startTime = Date.now();
-            timerIntervals[team] = setInterval(() => {
-                timers[team] = Math.floor((Date.now() - startTime) / 1000);
-                io.emit('update-timer', { team, time: timers[team] });
-            }, 1000);
-        }
-    });
-
-    socket.on('stop-timer', (team) => {
-        clearInterval(timerIntervals[team]);
-        timerIntervals[team] = null;
-    });
-
+    // Handle game addition
     socket.on('add-game', (gameName) => {
-        if (!games.includes(gameName)) {
-            games.push(gameName);
-            io.emit('update-games', games);
-        }
+        games.push(gameName);
+        io.emit('update-games', games);
+    });
+
+    // Handle timer updates
+    socket.on('update-timer', (data) => {
+        timers[data.team] = data.time;
+        io.emit('update-timer', data);
+    });
+
+    // Handle timer stop
+    socket.on('stop-timer', (data) => {
+        timers[data.team] = data.time;
+    });
+
+    // Handle checkbox updates
+    socket.on('update-checkbox', (data) => {
+        io.emit('update-checkbox', data);
+    });
+
+    // Handle game deletion
+    socket.on('delete-game', (gameName) => {
+        games = games.filter(game => game !== gameName);
+        io.emit('update-games', games);
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        console.log('A user disconnected');
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server listening on port 3000');
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
